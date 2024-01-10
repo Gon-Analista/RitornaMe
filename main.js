950c53b8babd3245032d568c8a2b320db96ca8b0
Gonzalo <go.ulloa@duocuc.cl>
2024-01-10 04:23:54 -0300

    test

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("loader-wrapper").style.display = "none";
-    document.getElementById("content").classList.remove("d-none");
-  }, 3000);
+  var images = document.querySelectorAll('.loader-logo, #menu-image-1, #menu-image-2');
+  var loadedImages = 0;
+
+  function imageLoaded() {
+    loadedImages++;
+    if (loadedImages === images.length) {
+      document.getElementById("loader-wrapper").style.display = "none";
+      document.getElementById("content").classList.remove("d-none");
+    }
+  }
+  images.forEach(function (img) {
+    img.addEventListener('load', imageLoaded);
+    img.addEventListener('error', imageLoaded); 
+  });
+
   document.querySelector('.loader-logo-container').style.clipPath = 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)';
   document.querySelector('.loader-logo').style.opacity = '1';
 });
